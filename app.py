0d0fbd52ce241498a16144aa739706e4307ae688
Bianca Tavares <83438587+biancatav@users.noreply.github.com>
2025-07-27 01:42:49 -0300

    limpando app.py
    ajustando código

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import joblib
 import numpy as np
-import pandas as pd  # ← essa linha é a correção!
+import pandas as pd
 
 st.set_page_config(page_title="Previsão de Obesidade", layout="centered")
 
